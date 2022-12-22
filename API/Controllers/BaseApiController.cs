5f379f2c67bf30e1f2bbc24c3e4aeb7c77c3ccf0
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2022-12-22 17:46:18 +1300

    add more http requests

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
-
+using MediatR;
 namespace API.Controllers
 {
   [ApiController]
   [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+        private IMediator _mediator;
+
+        // just in case, if _mediator exist, use it; if not exisit, use HttpContext
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
